e87bfa781c256d4d937a7b07c88a76514be07a7c
Gabriel Silva <gabriel127.gk@gmail.com>
2022-07-06 06:48:48 -0300

    Finalização endpoint: cadasterUser.

--- src/controllers/usersControllers/createCadasterUserController.ts
+++ src/controllers/usersControllers/createCadasterUserController.ts
@@ -3,21 +3,37 @@ import { Request, Response } from "express";
 import { CreateCadasterUserRepository } from "../../repositories/CreateCadasterUserRepository";
 import { CreateCadasterUserCase } from "../../use-cases/UsersCases/CreateCadasterUserCase";
 
+import { JwtAdapter } from "../../adapters/Jwt/Jwt-adapter";
+import { BCryptAdapter } from "../../adapters/Bcrypt/Bcrypt-adapter";
+import { NodemailerMailAdapter } from "../../adapters/Nodemailer/Nodemailer-adapter";
+
 export const createCadasterUserController = async ( req: Request, res: Response ): Promise<{}> => {
-    const { name, email, password, role } = req.body;
+    let { name, email, password, role } = req.body;
 
     const createCadasterUserRepository = new CreateCadasterUserRepository();
 
+    const jwtAdapter = new JwtAdapter();
+    const bcryptAdapter = new BCryptAdapter();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
+
     const createCadasterUserCase = new CreateCadasterUserCase(
-      createCadasterUserRepository
+      createCadasterUserRepository,
+      jwtAdapter,
+      bcryptAdapter,
+      nodemailerMailAdapter
     );
 
-    await createCadasterUserCase.createCadaster({
+    if ( role ) role = role.toUpperCase();
+
+    const tokenSuccess = await createCadasterUserCase.createCadaster({
       name,
       email,
       password,
       role
     });
     
-    return res.status(201).json({ message: `Usuário ${name} criado com sucesso.` });
+    return res.status(201).json({ 
+      message: `Usuário ${name} criado com sucesso.`,
+      token: tokenSuccess
+    });
 };
